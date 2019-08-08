51d3a0ed221fd548bbb619e829836a4d3f2b8f6c
gaolinfang <gaolinfang@unicloud.com>
2019-08-08 09:22:56 +0800

    新增de'mo

--- demo/字典.py
+++ demo/字典.py
@@ -1,8 +1,10 @@
 if __name__ == '__main__':
     dict1 = {'1':'壹','2':'贰'}
+    print(type(dict1))
     for item in dict1.keys():
         print(item)
     for item in dict1.values():
         print(item)
     for item in dict1.items():
         print(item)
+
