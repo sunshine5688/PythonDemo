c4c8ad1cd10caac6e9d6fbd0bd2843a4162191bd
gaolinfang <gaolinfang@unicloud.com>
2019-08-09 09:49:35 +0800

    新增de'mo

--- demo/继承.py
+++ demo/继承.py
@@ -2,13 +2,16 @@ class Parent:
     def hello(self):
         print('正在调用父类的方法')
 
-class Child(Parent): # 集成了Parent
+class Child(Parent): # 集成了Parent  python 可以多集成
     pass
 
 class Child2(Parent):
     def hello(self):
         print('调用child2的hello方法，覆盖了父类的hello方法')
         super().hello()  # 调用父类方法
+
+class Child3(Parent, Child2):
+    super().hello()  # 调用父类方法  尽量避免多继承
 
 if __name__ == '__main__':
     c = Child()
